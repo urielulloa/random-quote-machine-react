08b5a543c75077b64ed0f752955019fa4d9d449e
Uriel Ulloa <uulloaadan@middlebury.edu>
2018-09-11 23:56:59 -0700

    Tweet feature added
    * Added feature to allow users to tweet their favorite quotes.
    * Page now loads with a random quote instead of being empty.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 const quoteURL =
   "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+const tweetIntent = "https://twitter.com/intent/tweet?text=";
 
 import React from "react";
 import ReactDOM from "react-dom";
+
+class TwitterButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: this.props.quote,
+      author: this.props.author
+    };
+    this.createTweetURI = this.createTweetURI.bind(this);
+  }
+  createTweetURI() {
+    return (
+      tweetIntent +
+      encodeURIComponent(
+        '"' + this.state.quote + '"' + "- " + this.state.author
+      )
+    );
+  }
+  render() {
+    const tweetLink = this.createTweetURI();
+    return (
+      <a class="twitter-share-button" href={tweetLink}>
+        Tweet
+      </a>
+    );
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -10,15 +38,17 @@ class App extends React.Component {
     this.state = {
       quote: "",
       author: "",
-      quotes: []
+      quotes: [],
+      authors: [],
+      tweetLink: ""
     };
     this.getRandomQuote = this.getRandomQuote.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   getRandomQuote() {
-    const quotes = this.state.quotes.map(a => a.quote);
-    const authors = this.state.quotes.map(a => a.author);
+    const quotes = this.state.quotes;
+    const authors = this.state.authors;
     const index = Math.floor(Math.random() * quotes.length);
     const newQuote = quotes[index];
     const newAuthor = authors[index];
@@ -27,35 +57,53 @@ class App extends React.Component {
       author: newAuthor
     });
   }
-
+  createTweetURI() {
+    this.setState({
+      tweetLink:
+        tweetIntent +
+        encodeURIComponent('"' + this.state.quote + '" - ' + this.state.author)
+    });
+  }
   handleClick() {
     this.getRandomQuote();
+    this.createTweetURI();
   }
   componentDidMount() {
     fetch(quoteURL)
       .then(response => response.json())
       .then(responseJson => {
-        return this.setState({ quotes: responseJson.quotes.map(a => a) });
+        return (
+          this.setState({
+            quotes: responseJson.quotes.map(a => a.quote),
+            authors: responseJson.quotes.map(a => a.author)
+          }),
+          this.handleClick()
+        );
       })
       .catch(error => {
         console.error(error);
       });
   }
-
   render() {
     return (
       <div>
         {this.state.quote === "" ? (
-          <div>
+          <div id="quote-box">
             <p />
           </div>
         ) : (
-          <div>
-            <p>"{this.state.quote}"</p>
-            <p>- {this.state.author}</p>
+          <div id="quote-box">
+            <p id="text">"{this.state.quote}"</p>
+            <p id="author">- {this.state.author}</p>
           </div>
         )}
-        <button onClick={this.handleClick}>Get random quote</button>
+        <button id="new-quote" onClick={this.handleClick}>
+          Get random quote
+        </button>
+        <br />
+        <a class="twitter-share-button" href={this.state.tweetLink}>
+          Tweet
+        </a>
       </div>
     );
   }
